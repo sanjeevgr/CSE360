a639e973e7d35438bc2ba0d3ca6d804c380e95c5
Sanjeev Ganga Raju <san.gr.nc@gmail.com>
2020-10-06 22:59:12 -0700

    First commit (master) for Exercise 1 in CSE360 (AddingMachine.java)

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+/**
+* The Adding Machine class has several methods 
+* that model basic operations.
+* @author  Sanjeev Ganga Raju
+* @version 1.0
+* @since   2020-10-06
+*/
+public class AddingMachine {
+	private int total;
+	/**
+	 * This is the class constructor
+	 */
+	public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * getTotal returns value of total
+	 * @return 0
+	 */
+	public int getTotal () {
+	    return 0;
+	}
+	/**
+	 * add method adds values
+	 * @param value
+	 */
+	public void add (int value) {
+	}
+	/**
+	 * subtract method subtracts values
+	 * @param value
+	 */
+	public void subtract (int value) {
+	}
+	/**
+	 * toString method return string type of result
+	 * @return empty string
+	 */
+	public String toString () {
+	    return "";
+	}
+	/**
+	 * clear method 
+	 */
+	public void clear() {
+	}
+}
